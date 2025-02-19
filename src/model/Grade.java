8ac30a861dad8aa36bd1d63f27c5051bcac104ea
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-19 15:54:56 +0200

    Add: created Grade class with variables and getters and setter

---
+++ src/model/Grade.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Grade {
+	//1.variables
+	private long gId;
+	private int grValue;
+	private Student student;
+	private Course course;
+		
+	private static long counter = 30000;
+	
+	//2.getters
+	public long getgId() {
+		return gId;
+	}
+	public int getGrValue() {
+		return grValue;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public Course getCourse() {
+		return course;
+	}
+	
+	//3.setters
+	public void setGId() {
+		gId = counter++;
+	}
+	
+	public void setGrValue(int inputGrValue) {
+		if(inputGrValue >= 1 && inputGrValue <= 10) {
+			grValue = inputGrValue;
+		}
+		else
+		{
+			grValue = 0;
+		}
+	}
+	
+	public void setStudent(Student inputStudent) {
+		if(inputStudent != null) {
+			student = inputStudent;
+		}
+		else
+		{
+			student = new Student();
+		}
+	}
+	
+	public void setCourse(Course inputCourse) {
+		if(inputCourse != null) {
+			course = inputCourse;
+		}
+		else
+		{
+			course = new Course();
+		}
+	}
+
+}
